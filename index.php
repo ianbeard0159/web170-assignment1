fbf8eb8c567e68391dc3563e073c2b6cf4e135ce
Ian Beard <ianbeard0159@gmail.com>
2019-07-02 12:05:25 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,96 @@
+<!doctype HTML>
+<html lang="en">
+    <head>
+        <link rel="stylesheet" href="stylesheets/index.css">
+        <title>WEB-170 Assignment 1</title>
+    </head>
+    <body class="flex column">
+        <!-- Nav Bar -->
+        <header class="flex bar">
+            <div class="logo"></div>
+            <div class="flex" id="icon-container">
+                <div class="icon"></div>
+                <div class="icon"></div>
+                <div class="icon"></div>
+            </div>
+        </header>
+
+        <!-- Main  Container -->
+        <div class="flex column wrapper" id="main-container">
+            <!-- Main Picture Box -->
+            <div class="box" id="main-picturebox">
+            </div>
+
+            <div class="flex box column textbox">
+                <h2>WOW! An H2 Element!</h2>
+                <p>It even has content!
+                    This is a paragraph, look at how much of a paragraph this is.
+                    This is a paragraph, look at how much of a paragraph this is.
+                    This is a paragraph, look at how much of a paragraph this is.</p>
+            </div>
+
+            <!-- Article Container -->
+            <div class="flex container" id="article-container">
+                <!-- Article 1 -->
+                <div class="flex column article">
+                    <div class=article-picture></div>
+                    <div class="article-preview">
+                        <div class="preview-cover"></div>
+                        <div class="preview-content">
+                            Preview Content<br>
+                            This is a paragraph, look at how much of a paragraph this is.
+                            This is a paragraph, look at how much of a paragraph this is.
+                            This is a paragraph, look at how much of a paragraph this is.
+                        </div>
+                    </div>
+                </div>
+                <!-- Article 2 -->
+                <div class="flex column article">
+                    <div class=article-picture></div>
+                    <div class="article-preview">
+                        <div class="preview-cover"></div>
+                        <div class="preview-content">
+                            Preview Content<br>
+                            This is a paragraph, look at how much of a paragraph this is.
+                            This is a paragraph, look at how much of a paragraph this is.
+                            This is a paragraph, look at how much of a paragraph this is.
+                        </div>
+                    </div>
+                </div>
+                <!-- Article 3 -->
+                <div class="flex column article">
+                    <div class=article-picture></div>
+                    <div class="article-preview">
+                        <div class="preview-cover"></div>
+                        <div class="preview-content">
+                            Preview Content<br>
+                            This is a paragraph, look at how much of a paragraph this is.
+                            This is a paragraph, look at how much of a paragraph this is.
+                            This is a paragraph, look at how much of a paragraph this is.
+                        </div>
+                    </div>
+                </div>
+                <!-- Article 4 -->
+                <div class="flex column article">
+                    <div class=article-picture></div>
+                    <div class="article-preview">
+                        <div class="preview-cover"></div>
+                        <div class="preview-content">
+                            Preview Content<br>
+                            This is a paragraph, look at how much of a paragraph this is.
+                            This is a paragraph, look at how much of a paragraph this is.
+                            This is a paragraph, look at how much of a paragraph this is.
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <!-- Footer -->
+        <footer class="wrapper">
+            <div class="footer-container"></div>
+            <div class="flex bar validation-bar"></div>
+        </footer>
+    </body>
+</html>
